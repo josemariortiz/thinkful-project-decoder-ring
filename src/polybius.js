abb20906412ce03f2cdce5bf86e6400ac5c2999a
josemariortiz <josemariortiz@gmail.com>
2023-10-18 16:46:03 -0700

    Polybius function - Encode part finished

--- src/polybius.js
+++ src/polybius.js
@@ -6,8 +6,41 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+  const polySquare = [
+    ["a", "b", "c", "d", "e"],
+    ["f", "g", "h", "i/j", "k"],
+    ["l", "m", "n", "o", "p"],
+    ["q", "r", "s", "t", "u"],
+    ["v", "w", "x", "y", "z"],
+  ];
+
   function polybius(input, encode = true) {
     // your solution code here
+
+    const message = input.toLowerCase().split("");
+    let result = "";
+
+    if (encode) {
+      for (let i = 0; i < message.length; i++) {
+        const char = message[i];
+
+        if (char === " ") {
+          result += " ";
+        } else {
+          for (let row = 0; row < polySquare.length; row++) {
+            for (let col = 0; col < polySquare[row].length; col++) {
+              if (polySquare[row][col].includes(char)) {
+                const rowNumber = row + 1;
+                const colNumber = col + 1;
+
+                result += colNumber.toString() + rowNumber.toString();
+              }
+            }
+          }
+        }
+      }
+    }
+    return result;
   }
 
   return {
